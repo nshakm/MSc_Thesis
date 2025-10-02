3fc389f8c0eb5e4571ef4ce4e8a3d559a4070a02
nshakm <102352451+nshakm@users.noreply.github.com>
2025-10-02 10:43:11 +0200

    Clean up code by removing commented sections
    Removed commented-out code and unused variables for clarity.

--- 1_thermal_evolution_generation.py
+++ 1_thermal_evolution_generation.py
@@ -43,66 +43,7 @@ Tint_array = np.array([40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500])
 n_mrel = len(CMFs) * len(Zenvs) * len(mass) * len(Teqpls)
 print(n_mrel)
 
-# %%
-# #(potential) bad parameters for interior
-# CMFs = np.array([0.2, 0.3, 0.4, 0.5, 0.6, 0.7])[:1]
-
-# Z0 = 0.014
-# # Zenvs = (np.array([1e-2, 1e-1, 1e0, 1e1, 1e2])*Z0)[:2]
-# Zenvs = (np.array([0.01, 0.1, 1, 10])*Z0)[3:4]
-
-
-# mass = (np.concatenate([
-#     np.array([0.06, 0.1]),
-#     np.arange(0.15, 1, 0.05),
-#     np.arange(1, 2, 0.1),
-#     np.arange(2, 3, 0.2)
-# ])*Mjup)[:2]
-# # mass
-
-# Teqpls = np.array([400, 500, 600, 700, 800, 900, 1000])[:2]
-
-# #internal temperatures
-# Tint_array = np.array([50, 100, 200, 300, 400, 500])[:2]
-# # Tint_array = np.array([400, 500])
-
-# n_mrel = len(CMFs) * len(Zenvs) * len(mass) * len(Teqpls)
-# print(n_mrel)
-
-# # %%
-# #(potential) bad parameters for interior
-# CMFs = np.array([0.2, 0.3])
-
-# Z0 = 0.014
-# # Zenvs = (np.array([1e-2, 1e-1, 1e0, 1e1, 1e2])*Z0)[:2]
-# Zenvs = (np.array([0.01, 0.1, 1, 10])*Z0)[2:3]
-
-
-# mass = (np.concatenate([
-#     np.array([0.06, 0.1]),
-#     np.arange(0.15, 1, 0.05),
-#     np.arange(1, 2, 0.1),
-#     np.arange(2, 3, 0.2)
-# ])*Mjup)[3:5]
-# # print(mass)
-
-# Teqpls = np.array([400, 500, 600, 700, 800, 900, 1000])[:1]
-
-# #internal temperatures
-# Tint_array = np.array([50, 100, 200, 300, 400, 500])[2:4]
-# # Tint_array = np.array([400, 500])
-
-# n_mrel = len(CMFs) * len(Zenvs) * len(mass) * len(Teqpls)
-# print(n_mrel)
-
-
-# %%
-
-#Initialize the mask array with zeros
-# mask = np.zeros((len(CMFs), len(Zenvs), len(mass), len(Teqpls)), dtype=int)
-mask = np.ones((len(CMFs), len(Zenvs), len(mass), len(Teqpls), 5), dtype=float)
-# mask
-
+##
 # %%
 #loop for thermal evolution
 counter = 0
@@ -142,9 +83,9 @@ for i_cmf, CMF in enumerate(CMFs):
                     print('---------------')
                     print(f"CMF = {CMF:.2f}")
                     print(f"Zenv = {Zenv:.1e}")
-                    print(f"Mass = {M_P:.2f}")
+                    print(f"Mass [Mearth] = {M_P:.2f}")
                     print(f"Teq = {Teqpl:.2f}")
-                    print(f"Sequence = {counter} out of {n_mrel}")
+                    print(f"Sequence [K] = {counter} out of {n_mrel}")
                     print('---------------')
 
                     #thermal class evolution object
@@ -177,47 +118,10 @@ for i_cmf, CMF in enumerate(CMFs):
                         print("interior_retrieval.py: Thermal evolution equation solved")
                         print("----------------------------------------------------")
 
-                        # 42/0
-                        
-                        # # # If successful, set the success flag to 1
-                        # # mask[i_cmf, i_Zenv, i_mass, i_teq, 4] = 1
-                        # print(f"interior_retrieval.py: mask_flag = {mask_flag}")
-                        # mask_flag = True
-                        # print(f"interior_retrieval.py: mask_flag = {mask_flag}")
-                        # # print("interior_retrieval.py: Success flag set to 1")
                     
                     except ValueError as e:
                         print(f"interior_retrieval.py: Error in sequence -> {e}")
-                        # print("interior_retrieval.py: 1st line")
-                        # raise ValueError('Error in sequence')
-                        # print(type(my_therm_obj.f_S))
-                        # print('Error in sequence')
-                        # print("2nd line")
-                        
-                        # #Save sequence of interior models with NaNs
-                        # data = np.zeros((len(my_therm_obj.f_S), 11))
-                        # print("Data initialized with shape: ", data.shape)
-                        # data[:,0] = np.nan #my_therm_obj.f_S
-                        # data[:,1] = np.nan #my_therm_obj.s_mean_TE
-                        # data[:,2] = np.nan #my_therm_obj.s_top_TE 
-                        # data[:,3] = np.nan #my_therm_obj.Tint_array
-                        # data[:,4] = 5 #my_therm_obj.Rtot_TE*Rjup
-                        # data[:,5] = np.nan #my_therm_obj.Rbulk_TE*Rjup
-                        # data[:,6] = np.nan #my_therm_obj.Tsurf_TE
-                        # data[:,7] = np.nan #my_therm_obj.age_points
-                        # data[:,8] = np.nan #my_therm_obj.Zenv_TE
-                        # data[:,9] = np.nan #my_therm_obj.Mtot_TE
-                        # # tmm = CMF + (1-CMF)*my_therm_obj.Zenv_TE
-                        # data[:,10] = np.nan #tmm
-                        # header0 = 'f_S s_mean_TE s_top_TE Tint_K Rtot_earth Rbulk_earth Tsurf_K Age_Gyrs Zenv Mtot_earth Zplanet'
-
-                        # np.savetxt(file_name, data, header=header0, comments='', fmt='%1.4e')
-                        # print("File saved")
-                        # continue
-
-                    if np.any(np.isnan(my_therm_obj.f_S)):
-                        # If successful, set the success flag to 1
-                        mask[i_cmf, i_Zenv, i_mass, i_teq, 4] = 0
+                    
 
                     #Save sequence of interior models
                     data = np.zeros((len(my_therm_obj.f_S), 11))
@@ -239,75 +143,3 @@ for i_cmf, CMF in enumerate(CMFs):
 
 
 print(f"dummy2.py: successful for {CMF}")
-#  %%
-# my_therm_obj.age_points
-
-
-# %%
-
-# #mask analysis
-# print(f"mask:\n {mask} \n")
-# print('-----')
-
-# print(f"mask[..., 4]: \n {mask[..., 4]} \n")
-# print('-----')
-
-# print(f"mask[..., 4] == 1: \n {mask[..., 4] == 1} \n")
-# print('-----')
-# # mask[..., 4] == 1
-
-# #successful indices
-# successful_indices = np.argwhere(mask[..., 4] == 1)
-# print(f"successful indices: \n {successful_indices} \n")
-# # successful_indices
-
-# # Extract parameters for successful runs
-# successful_parameters = []
-# for idx in successful_indices:
-#     # print(idx[0])
-#     CMF = mask[idx[0], idx[1], idx[2], idx[3], 0]
-#     # print(CMF)
-#     Zenv = mask[idx[0], idx[1], idx[2], idx[3], 1]
-#     # print(Zenv)
-#     mass = mask[idx[0], idx[1], idx[2], idx[3], 2]
-#     # print(mass)
-#     Teqpl = mask[idx[0], idx[1], idx[2], idx[3], 3]
-#     # print(Teqpl)
-#     successful_parameters.append((CMF, Zenv, mass, Teqpl))
-#     print(successful_parameters)
-
-# print("Successful parameter combinations:")
-# for params in successful_parameters:
-#     print(f"CMF = {params[0]:.2f}, Zenv = {params[1]:.2e}, Mass = {params[2]:.2f}, Teq = {params[3]:.2f}")
-
-# # # %%
-# # # Save the mask array to a file
-# # flattened_mask = mask.reshape(-1, mask.shape[-1])
-# # # flattened_mask
-
-# # #header for the file
-# # header = 'CMF Zenv M_P [M_E] Teq [K] Success'
-
-# # # Save the flattened mask as a table
-# # np.savetxt("mask_table.txt", flattened_mask, header=header, comments='', fmt='%1.4e')
-
-# # print("Mask saved as 'mask_table.txt'")
-
-# # %%
-# import pandas as pd
-
-# flattened_mask = mask.reshape(-1, mask.shape[-1])
-
-# # Convert the mask to a DataFrame
-# columns = ["CMF", "Zenv", "M_P [M_E]", "Teq [K]", "Success"]
-# df = pd.DataFrame(flattened_mask, columns=columns)
-
-# # Save as a CSV file
-
-# table_name = f"mask_table_CMF_{str({CMFs[0]})}.csv"
-# df.to_csv(table_name, index=False)
-
-# print(f"Mask saved as '{table_name}'")
-
-# # np.save("mask.npy", mask)
-# %%
